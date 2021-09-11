1a97f568c383ac22b29ba3445de924f26fd3eee7
Ahmad Ali <85236408+SantyAli@users.noreply.github.com>
2021-09-11 04:31:01 -0700

    ajaxversioncommit

---
+++ src/Components/UI/Spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Spinner.css";
+const Spinner = () => {
+  return (
+    <div className="Loader">
+      <div class="loader">Loading...</div>
+    </div>
+  );
+};
+
+export default Spinner;
---
+++ src/hoc/WithErrorHandler/WithErrorHandler.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Component } from "react";
+import Auxx from "../Auxx";
+import Modal from "../../Components/UI/Modal/Modal";
+const WithErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null,
+    };
+    componentWillMount() {
+      this.reqInterceptor = axios.interceptors.request.use((req) => {
+        this.setState({ error: null });
+        return req;
+      });
+      this.resInterceptor = axios.interceptors.response.use(
+        (res) => res,
+        (error) => {
+          this.setState({ error: error });
+        }
+      );
+    }
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.respose.eject(this.resInterceptor);
+    }
+
+    // componentDidMount() {
+    //   axios.interceptors.request.use((req) => {
+    //     this.setState({ error: null });
+    //     return req;
+    //   });
+    //   axios.interceptors.response.use(
+    //     (res) => res,
+    //     (error) => {
+    //       this.setState({ error: error });
+    //     }
+    //   );
+    // }
+    errorConfirmedHandler = () => {
+      this.setState({ error: null });
+    };
+    render() {
+      return (
+        <Auxx>
+          <Modal
+            show={this.state.error}
+            modalClosed={this.errorConfirmedHandler}
+          >
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Auxx>
+      );
+    }
+  };
+};
+
+export default WithErrorHandler;
